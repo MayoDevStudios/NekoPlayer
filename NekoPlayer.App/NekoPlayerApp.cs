48bec1660c56821d9f6af1cf8566ae27cf9a13e0
codefactor-io <support@codefactor.io>
2026-03-08 05:27:17 +0000

    [CodeFactor] Apply fixes

--- NekoPlayer.App/NekoPlayerApp.cs
+++ NekoPlayer.App/NekoPlayerApp.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2026 BoomboxRapsody <boomboxrapsody@gmail.com>. Licensed under the MIT Licence.
+// Copyright (c) 2026 BoomboxRapsody <boomboxrapsody@gmail.com>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 #nullable disable
@@ -337,7 +337,6 @@ namespace NekoPlayer.App
 
         public virtual void RequestHideToTrayIcon()
         {
-
         }
 
         public bool QuitRequested;

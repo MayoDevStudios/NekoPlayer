fd8557806302e4f3003cebc9361843102f210a9d
BoomboxRapsody <seonghyeon802@gmail.com>
2026-03-12 20:59:17 +0900

    Update NekoPlayerAppBase.cs

--- NekoPlayer.App/NekoPlayerAppBase.cs
+++ NekoPlayer.App/NekoPlayerAppBase.cs
@@ -343,7 +343,7 @@ namespace NekoPlayer.App
 
                 dependencies.CacheAs(colours = new AdaptiveColour());
 
-                dependencies.CacheAs(overlayColourProvider = new OverlayColourProvider(OverlayColourScheme.Blue));
+                dependencies.CacheAs(overlayColourProvider = new OverlayColourProvider(OverlayColourScheme.Lime));
 
                 Logger.Log($"🎨 OverlayColourProvider loaded");
 
